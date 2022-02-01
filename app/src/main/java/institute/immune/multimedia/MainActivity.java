6148f90d4468cd78b0e0ecd3ceaebaa625230e92
Dani <daniel.gonzalez@immune.institute>
2022-02-01 12:21:06 +0100

    He hecho muchas cosas

--- app/src/main/java/institute/immune/multimedia/MainActivity.java
+++ app/src/main/java/institute/immune/multimedia/MainActivity.java
@@ -2,14 +2,36 @@ package institute.immune.multimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.cancion);
     }
-    //holaHola hola holaHOLAAAA
+
+    public void pause(View view) {
+        Toast.makeText(this, "Se ha pausado la canción", Toast.LENGTH_SHORT).show();
+        mp.pause();
+
+    }
+    public void play(View view) {
+        Toast.makeText(this, "Se ha iniciado la canción", Toast.LENGTH_SHORT).show();
+        mp.start();
+
+    }
+
+    public void stop(View view) {
+        Toast.makeText(this, "Se ha parado la canción", Toast.LENGTH_SHORT).show();
+        mp.stop();
+        mp = MediaPlayer.create(this, R.raw.cancion);
+    }
+
+
 }
